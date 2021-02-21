42ec61fb26706d86b072b17ae8c625569eecc21d
vintzvintz <git@vintz.fr>
2021-02-21 17:48:11 +0100

    tests

--- root/etc/cont-init.d/55-set-ports.sh
+++ root/etc/cont-init.d/55-set-ports.sh
@@ -3,10 +3,15 @@
 
 PORTS_RC="/config/rtorrent/ports.rc"
 
-if [[ ! -e "$PORTS_RC" ]];
-then
+echo "$(env)"
+
+if [[ ! -e "$PORTS_RC" ]]; then
+  echo "Conserve ports.rc existant"
+else
   [[ -z "$BT_PORT"  ]] && BT_PORT=50000
   [[ -z "$DHT_PORT" ]] && DHT_PORT=6881
   echo "dht.port.set=${DHT_PORT}" > "$PORTS_RC"
   echo "network.port_range.set = ${BT_PORT}-${BT_PORT}" >> "$PORTS_RC"
 fi
+
+cat $PORTS_RC
